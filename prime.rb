f2c9349ebc581a8de77c80b621e3f23976ad0c4c
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 22:39:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
-def prime?(num)
-num.each do [i]
-  if i.prime?
-    return true
-  else
-    return false
-end
-end
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
